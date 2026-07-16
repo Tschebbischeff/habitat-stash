d32b3c06e5e2aa83614c46edfbbb8314777cb5fe
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-07-16 05:42:12 +0200

    implements un-/installing custom apps and un-/setting system config via environment variables

---
+++ habitat-config/nextcloud/before-starting/custom-apps.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+MANDATORY_CUSTOM_APPS="oidc_login"
+
+shopt -s extglob
+
+inArray() {
+    local search="$1"; shift
+    for item in "$@"; do
+        [[ "$item" == "$search" ]] && return 0
+    done
+    return 1
+}
+
+NEXTCLOUD_CUSTOM_APPS="$MANDATORY_CUSTOM_APPS,$NEXTCLOUD_CUSTOM_APPS"
+declare -a REQ_APPS
+mapfile -t REQ_APPS < <(printf "%s" "$NEXTCLOUD_CUSTOM_APPS" | sed -E 's/([^\\]),/\1\n/g')
+for i in "${!REQ_APPS[@]}"; do
+    REQ_APPS[i]="${REQ_APPS[i]##+[[:space:]]}"
+    REQ_APPS[i]="${REQ_APPS[i]%%+[[:space:]]}"
+done
+
+INSTALLED_APPS="$(php occ app:list --no-interaction --no-warnings --shipped=false --enabled --output=plain | grep -Po ' *- *\K[^:]*')"
+
+for app in "${REQ_APPS[@]}"; do
+    [ -n "$app" ] || continue
+    if ! inArray "$app" "${INSTALLED_APPS[@]}"; then
+        echo "Installing custom app '$app'..."
+        php occ app:install --no-interaction "$app"
+    else
+        echo "Updating custom app '$app'..."
+        php occ app:update --no-interaction "$app"
+    fi
+done
+
+for app in "${INSTALLED_APPS[@]}"; do
+    [ -n "$app" ] || continue
+    if ! inArray "$app" "${REQ_APPS[@]}"; then
+        echo "Uninstalling custom app '$app'..."
+        php occ app:remove --no-interaction "$app"
+    fi
+done
---
+++ habitat-config/nextcloud/before-starting/system-config.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+cat >"$MANDATORY_SYSTEM_CONFIG_SET" <<EOF
+oidc_login_provider_url="https://authelia.${APP_HOST}"
+allow_user_to_change_display_name=false
+EOF
+
+cat >"$MANDATORY_SYSTEM_CONFIG_UNSET" <<EOF
+EOF
+
+shopt -s extglob
+
+inArray() {
+    local search="$1"; shift
+    for item in "$@"; do
+        [[ "$item" == "$search" ]] && return 0
+    done
+    return 1
+}
+
+NEXTCLOUD_SYSTEM_CONFIG_SET="$MANDATORY_SYSTEM_CONFIG_SET"$'\n'"$NEXTCLOUD_SYSTEM_CONFIG_SET"
+NEXTCLOUD_SYSTEM_CONFIG_UNSET="$MANDATORY_SYSTEM_CONFIG_UNSET"$'\n'"$NEXTCLOUD_SYSTEM_CONFIG_UNSET"
+
+declare -a CONFIG_SET
+mapfile -t CONFIG_SET < <(printf "%s" "$NEXTCLOUD_SYSTEM_CONFIG_SET" | sed -E 's/([^\\]),/\1\n/g')
+for i in "${!CONFIG_SET[@]}"; do
+    CONFIG_SET[i]="${CONFIG_SET[i]##+[[:space:]]}"
+    CONFIG_SET[i]="${CONFIG_SET[i]%%+[[:space:]]}"
+done
+
+declare -a CONFIG_UNSET
+mapfile -t CONFIG_UNSET < <(printf "%s" "$NEXTCLOUD_SYSTEM_CONFIG_UNSET" | sed -E 's/([^\\]),/\1\n/g')
+for i in "${!CONFIG_UNSET[@]}"; do
+    CONFIG_UNSET[i]="${CONFIG_UNSET[i]##+[[:space:]]}"
+    CONFIG_UNSET[i]="${CONFIG_UNSET[i]%%+[[:space:]]}"
+done
+
+for cfg in "${CONFIG_SET[@]}"; do
+    [ -n "$cfg" ] || continue
+    cfgName="$(echo "$cfg" | grep -Po '^[^ =]*')"
+    cfgValue="$(echo "$cfg" | grep -Po '^[^ =]*( |=)\K.*$')"
+    { [ -n "$cfgName" ] && [ -n "$cfgValue" ]; } || continue
+    currentValue="$(php occ config:system:get "$cfgName" --no-interaction --no-warnings --output=plain)"
+    [ "$cfgValue" != "$currentValue" ] || continue
+    if [ "$cfgValue" == "true" ] || [ "$cfgValue" == "false" ]; then
+        php occ config:system:set "$cfgName" --no-interaction "--value=$cfgValue" --type=boolean
+    elif [ "${value:0:1}" == "\"" ]; then
+        php occ config:system:set "$cfgName" --no-interaction "--value=$cfgValue" --type=string
+    elif [[ "$cfgValue" == *.* ]]; then
+        php occ config:system:set "$cfgName" --no-interaction "--value=$cfgValue" --type=double
+    else
+        php occ config:system:set "$cfgName" --no-interaction "--value=$cfgValue" --type=integer
+    fi
+done
+
+for cfgName in "${CONFIG_UNSET[@]}"; do
+    [ -n "$cfgName" ] || continue
+    currentValue="$(php occ config:system:get "$name" --no-interaction --no-warnings --output=plain)"
+    [ -n "$currentValue" ] || continue
+    php occ config:system:delete "$cfgName" --no-interaction
+done
