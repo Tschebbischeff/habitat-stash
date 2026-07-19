8ff528fee7f1f2fa3ca6e9bf9953a92257fba4b6
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-07-19 19:24:44 +0200

    switches to executing envsubst on all sql files under source path

--- habitat-config/.transform/mariadb.sh
+++ habitat-config/.transform/mariadb.sh
@@ -4,6 +4,10 @@ SOURCE_PATH="$1"
 
 export NEXTCLOUD_MYSQL_PASSWORD_HASH="$(cat "/run/secrets/NEXTCLOUD_MYSQL_PASSWORD_HASH")"
 
-envsubst <"$SOURCE_PATH/provisioning/nextcloud/.dbinit.sql" >"$SOURCE_PATH/provisioning/nextcloud/.dbinit.sql.tmp" \
-&& \
-mv "$SOURCE_PATH/provisioning/nextcloud/.dbinit.sql.tmp" "$SOURCE_PATH/provisioning/nextcloud/.dbinit.sql"
+find "$SOURCE_PATH" -type f -name '*.sql' | while read -r filePath; do
+    if envsubst <"$filePath" >"$filePath.envsubst"; then
+        mv "$filePath.envsubst" "$filePath"
+    else
+        rm "$filePath.envsubst" &>/dev/null
+    fi
+done
