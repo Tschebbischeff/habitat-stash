c45e1dcf8206b11a520a02f211839a2d46f2977f
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-07-20 02:13:22 +0200

    fixes checking for wrong file

--- nextcloud/entrypoint-hook.sh
+++ nextcloud/entrypoint-hook.sh
@@ -4,12 +4,12 @@ mkdir -p /run/secrets/www-data
 chown www-data:www-data /run/secrets/www-data
 chmod 755 /run/secrets/www-data
 
-[ -f "NEXTCLOUD_OAUTH_CLIENT_ID_FILE" ] && \
+[ -f "$NEXTCLOUD_OAUTH_CLIENT_ID_FILE" ] && \
     cat "$NEXTCLOUD_OAUTH_CLIENT_ID_FILE" >/run/secrets/www-data/NEXTCLOUD_OAUTH_CLIENT_ID && \
     chown -R www-data:www-data /run/secrets/www-data/NEXTCLOUD_OAUTH_CLIENT_ID && \
     chmod 400 /run/secrets/www-data/NEXTCLOUD_OAUTH_CLIENT_ID
 
-[ -f "NEXTCLOUD_OAUTH_CLIENT_SECRET_FILE" ] && \
+[ -f "$NEXTCLOUD_OAUTH_CLIENT_SECRET_FILE" ] && \
     cat "$NEXTCLOUD_OAUTH_CLIENT_SECRET_FILE" >/run/secrets/www-data/NEXTCLOUD_OAUTH_CLIENT_SECRET && \
     chown -R www-data:www-data /run/secrets/www-data/NEXTCLOUD_OAUTH_CLIENT_SECRET && \
     chmod 400 /run/secrets/www-data/NEXTCLOUD_OAUTH_CLIENT_SECRET
