6a158f5bd375f24ff1d2e4f0c20bd9424d2eb71e
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-08-29 05:49:57 +0200

    disables app before re-enabling them if arguments need to be set

--- habitat-config/nextcloud/before-starting/102-enable-apps.sh
+++ habitat-config/nextcloud/before-starting/102-enable-apps.sh
@@ -47,6 +47,7 @@ mapfile -t ENABLED_APPS < <(php occ app:list --no-interaction --no-warnings --en
 for appId in "${!APP_LIST[@]}"; do
     app="${APP_LIST[$appId]}"
     appArgs="${APP_ARGS[$appId]}"
+    doDisable=""
     doEnable=""
     if ! inArray "$app" "${ENABLED_APPS[@]}"; then
         echo "Enabling app '$app'..."
@@ -54,8 +55,10 @@ for appId in "${!APP_LIST[@]}"; do
     fi
     if [ -n "$appArgs" ]; then
         echo "Setting app arguments for '$app': $appArgs"
+        inArray "$app" "${ENABLED_APPS[@]}" && doDisable="_"
         doEnable="_"
     fi
+    [ -n "$doDisable" ] && php occ app:disable --no-interaction "$app"
     # shellcheck disable=SC2086  # Word-splitting is intentional
     [ -n "$doEnable" ] && php occ app:enable --no-interaction $appArgs "$app"
 done
