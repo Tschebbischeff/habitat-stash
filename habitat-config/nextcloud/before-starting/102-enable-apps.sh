2797c4578cbc4223144552a75853aa5f34a157dc
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-08-29 04:42:13 +0200

    supports arguments for the app:enable calls via env var NEXTCLOUD_ENABLE_APPS

--- habitat-config/nextcloud/before-starting/102-enable-apps.sh
+++ habitat-config/nextcloud/before-starting/102-enable-apps.sh
@@ -8,19 +8,54 @@ inArray() {
     return 1
 }
 
+declare -a APP_CONFIG
+mapfile -t APP_CONFIG < <(printf "%s" "$NEXTCLOUD_ENABLE_APPS" | sed -E 's/([^\\]|^),/\1\n/g')
+for i in "${!APP_CONFIG[@]}"; do
+    APP_CONFIG[i]="$(echo "${APP_CONFIG[i]}" | grep -Po '^[ \t]*\K.*[^ \t]')"
+done
+
+APP_ARGS_WHITELIST=( \
+    "groups" \
+    "force"
+)
 declare -a APP_LIST
-mapfile -t APP_LIST < <(printf "%s" "$NEXTCLOUD_ENABLE_APPS" | sed -E 's/([^\\]|^),/\1\n/g')
-for i in "${!APP_LIST[@]}"; do
-    APP_LIST[i]="$(echo "${APP_LIST[i]}" | grep -Po '^[ \t]*\K.*[^ \t]')"
+declare -a APP_ARGS
+for appConfig in "${APP_CONFIG[@]}"; do
+    [ -n "$appConfig" ] || continue
+    APP_LIST[${#APP_LIST[@]}]="$(echo "$appConfig" | grep -Po '^[^\[]*')"
+    appArgsStr=""
+    appArgs="$(echo "$appConfig" | grep -Po '^[^\[]*\K.*$' | sed -E 's/\]\[/\]\n\[/g')"
+    while [ -n "$appArgs" ]; do
+        arg="$(echo "$appArgs" | grep -Po '^\[?\K[^\]]*')"
+        argName="$(echo "$arg" | grep -Po '[^="]*')"
+        argValue="$(echo "$arg" | grep -Po '[^=]*\K[^"]*$')"
+        if inArray "$argName" "${APP_ARGS_WHITELIST[@]}"; then
+            if [ -z "$argValue" ]; then
+                appArgsStr="$appArgsStr \"$argName\""
+            else
+                appArgsStr="$appArgsStr \"$argName=$argValue\""
+            fi
+        fi
+        appArgs="$(echo "$appArgs" | grep -Po '^\[?[^\]]*\]\K.*')"
+    done
+    APP_ARGS[${#APP_ARGS[@]}]="$appArgsStr"
 done
 
 declare -a ENABLED_APPS
 mapfile -t ENABLED_APPS < <(php occ app:list --no-interaction --no-warnings --enabled --output=plain | grep -Po ' *- *\K[^:]*')
 
-for app in "${APP_LIST[@]}"; do
-    [ -n "$app" ] || continue
+for appId in "${!APP_LIST[@]}"; do
+    app="${APP_LIST[$appId]}"
+    appArgs="${APP_ARGS[$appId]}"
+    doEnable=""
     if ! inArray "$app" "${ENABLED_APPS[@]}"; then
         echo "Enabling app '$app'..."
-        php occ app:enable --no-interaction "$app"
+        doEnable="_"
     fi
+    if [ -n "$appArgs" ]; then
+        echo "Setting app arguments for '$app': $appArgs"
+        doEnable="_"
+    fi
+    # shellcheck disable=SC2086  # Word-splitting is intentional
+    [ -n "$doEnable" ] && php occ app:enable --no-interaction $appArgs "$app"
 done
